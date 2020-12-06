bb8c3ebb88d2b3ebd1f053080afd1c7d258233f3
Danilo <dansantosalfa@gmail.com>
2020-12-06 11:19:57 -0300

    Initial commit

---
+++ build/precache-manifest.8601cb056cde7d518b17c2b82801c624.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34858c256dac1d5762b9cdd0e3b5931d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98bd3541a5383898c733",
+    "url": "/static/js/2.9ced3a44.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.9ced3a44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8b719e737084ac2c1be",
+    "url": "/static/js/main.cb881d4e.chunk.js"
+  },
+  {
+    "revision": "fe617c2cb41aece9e721",
+    "url": "/static/js/runtime-main.e8df815d.js"
+  }
+]);
